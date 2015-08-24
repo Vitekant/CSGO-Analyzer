82e64548ef5e703e052cd99fb4b8e4daa41f1e9b
Witold Antkiewicz <vitekant@gmail.com>
2015-08-23 22:52:35 -0700

    Inital check-in. Frags calculation + entry frags attempt

---
+++ CSGO-Analyzer/CSGO-Analyzer/Analyzer.cs
@@ -0,0 +1,114 @@
+﻿using DemoInfo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGO_Analyzer
+{
+    public static class Analyzer
+    {
+        public static void Analyze(string demoPath)
+        {
+            using (var fileStream = File.OpenRead(demoPath))
+            {
+                Console.WriteLine("Parsing demo " + demoPath);
+                using (var parser = new DemoParser(fileStream))
+                {
+                    parser.ParseHeader();
+
+                    string map = parser.Map;
+
+                    Console.WriteLine("Map: " + map);
+
+                    bool hasMatchStarted = false;
+                    bool firstBlood = true;
+                    int round = 0;
+                    Dictionary<Player, int> killsThisRound = new Dictionary<Player, int>();
+                    Dictionary<Player, int> entryFrags = new Dictionary<Player, int>();
+                    Dictionary<Player, int> entryFragAttempts = new Dictionary<Player, int>();
+
+                    parser.MatchStarted += (sender, e) =>
+                    {
+                        hasMatchStarted = true;
+                    };
+
+                    parser.RoundStart += (sender, e) =>
+                    {
+                        if (!hasMatchStarted)
+                            return;
+                        firstBlood = true;
+                        round++;
+                    };
+
+                    parser.PlayerKilled += (object sender, PlayerKilledEventArgs e) => {
+
+                    if (!hasMatchStarted)
+                        return;
+                        //the killer is null if you're killed by the world - eg. by falling
+                        if (e.Killer != null)
+                        {
+                            if (!killsThisRound.ContainsKey(e.Killer))
+                                killsThisRound[e.Killer] = 0;
+
+                            if(e.Killer.Team == e.Victim.Team)
+                            {
+                                killsThisRound[e.Killer]--;
+
+                            }
+                            else
+                            {
+                                killsThisRound[e.Killer]++;
+                            }
+                            //Remember how many kills each player made this rounds
+
+                            if (firstBlood)
+                            {
+                                if (!entryFrags.ContainsKey(e.Killer))
+                                    entryFrags[e.Killer] = 0;
+
+                                entryFrags[e.Killer]++;
+
+                                if (!entryFragAttempts.ContainsKey(e.Killer))
+                                    entryFragAttempts[e.Killer] = 0;
+
+                                entryFragAttempts[e.Killer]++;
+
+                                if (!entryFragAttempts.ContainsKey(e.Victim))
+                                    entryFragAttempts[e.Victim] = 0;
+
+                                entryFragAttempts[e.Victim]++;
+
+                                firstBlood = false;
+                            }
+                        }
+                    };
+
+                    parser.ParseToEnd();
+
+                    foreach(var player in killsThisRound.Keys)
+                    {
+                        if (!entryFragAttempts.ContainsKey(player))
+                        {
+                            entryFragAttempts[player] = 0;
+                        }
+
+                        if (!entryFrags.ContainsKey(player))
+                        {
+                            entryFrags[player] = 0;
+                        }
+
+                        if (!killsThisRound.ContainsKey(player))
+                        {
+                            killsThisRound[player] = 0;
+                        }
+
+                        Console.WriteLine(player.Name + "(" + player.Team + ")" + " frags: " + killsThisRound[player] + "; entry frags: " + entryFrags[player] + "/" + entryFragAttempts[player]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSGO-Analyzer/CSGO-Analyzer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSGO_Analyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void selectDemoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    selectedDemoTextBox.Text = file;
+                    selectedDemoTextBox.ToolTip = file;
+                    Analyzer.Analyze(file);
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    selectedDemoTextBox.Text = null;
+                    selectedDemoTextBox.ToolTip = null;
+                    break;
+            }
+        }
+    }
+}
